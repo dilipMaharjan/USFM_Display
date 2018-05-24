4d04b233bfcd79a9305af75863e04556ffe6be67
dilip <dilipmaharjann@gmail.com>
2018-05-24 14:18:39 -0400

    chapterwise text display

--- src/main/kotlin/Controllers/Store.kt
+++ src/main/kotlin/Controllers/Store.kt
@@ -5,46 +5,47 @@ import Models.Chapter
 import Models.Verse
 import Views.TextView
 import javafx.beans.property.SimpleStringProperty
+import javafx.stage.FileChooser
 import tornadofx.*
 import java.io.File
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
-import javafx.stage.FileChooser
 
-class Store: Controller(){
+class Store : Controller() {
     lateinit var file: File
     val titleProperty = SimpleStringProperty("")
     var book = Book(titleProperty.value)
     val display = SimpleStringProperty("")
     val textView: TextView by inject()
 
-    fun addVerse(chapter: Chapter, text:String){
+    fun addVerse(chapter: Chapter, text: String) {
         var newVerse = Verse(text.removePrefix("\\v").trim())
         chapter.verses.add(newVerse)
         display.value += newVerse.text
     }
-    fun addChapter(book: Book, text:String){
+
+    fun addChapter(book: Book, text: String) {
         var newChapter = Chapter(text.removePrefix("\\c").trim())
         book.chapters.add(newChapter)
         display.value += newChapter.title
     }
 
-    fun changeTitle(text: String){
+    fun changeTitle(text: String) {
         this.book.titleProperty.value = text.removePrefix("\\h").trim() + "\n"
         display.value += book.title
     }
 
     //Remove footer tags from USFM file contents
-    fun String.formatText(): String{
-        if(this.contains("\\f"))
-            return this.removeRange(this.indexOf("\\f")..this.length-1)
+    fun String.formatText(): String {
+        if (this.contains("\\f"))
+            return this.removeRange(this.indexOf("\\f")..this.length - 1)
         return this
     }
 
-    fun parseText(text: String){
+    fun parseText(text: String) {
         val fileContents = text.split("\n")
-        loop@for(line in fileContents){
-            when{
+        loop@ for (line in fileContents) {
+            when {
                 line.startsWith("\\h") -> changeTitle(line)
                 line.startsWith("\\c") -> addChapter(book, line)
                 line.startsWith("\\v") -> addVerse(book.chapters.last(), line.formatText())
@@ -53,17 +54,24 @@ class Store: Controller(){
         }
     }
 
-    fun loadFile(parser: (File) -> String = { Files.readAllBytes(it.toPath()).toString(StandardCharsets.UTF_8)},
-                 filters: Array<FileChooser.ExtensionFilter>){
+    fun loadFile(
+        parser: (File) -> String = { Files.readAllBytes(it.toPath()).toString(StandardCharsets.UTF_8) },
+        filters: Array<FileChooser.ExtensionFilter>
+    ) {
         var fileContents = SimpleStringProperty()
-        chooseFile("Select USFM File", filters, FileChooserMode.Single).singleOrNull()?.let{
+        chooseFile("Select USFM File", filters, FileChooserMode.Single).singleOrNull()?.let {
             fileContents.value = parser.invoke(it)
             file = it
         }
-        if(fileContents.value != null)
+        if (fileContents.value != null)
             this.parseText(fileContents.value)
     }
-    enum class BackGroundTheme{light, dark}
+
+    enum class BackGroundTheme { light, dark }
+
+    fun getChapterNumber(string: String): Int {
+        return string.split(" ")[1].trim().toInt()
+    }
 }
 
 
--- src/main/kotlin/Views/BookView.kt
+++ src/main/kotlin/Views/BookView.kt
@@ -1,15 +1,46 @@
 package Views
 
-import tornadofx.*
 import Controller.Store
+import tornadofx.*
 
-class BookView: View(){
+class BookView : View() {
     val store: Store by inject()
-    override val root = vbox(){
+    var chapterNum = 2
+    override val root = vbox {
         useMaxWidth = true
+        prefHeight = 525.0
         scrollpane {
-            prefHeight = 525.0
-            text(store.display)
+            vbox {
+                text(store.book.title)
+                for (chap in store.book.chapters) {
+                    if (store.getChapterNumber(chap.title) == chapterNum) {
+                        text(chap.title)
+                        for (v in chap.verses) {
+                            text(v.text)
+                        }
+                        continue
+                    }
+                }
+            }
+        }
+        hbox {
+            button("<<<Prev") {
+                action {
+                    if (chapterNum > 1) chapterNum -= 1 else chapterNum = 1
+                }
+
+            }
+            button(chapterNum.toString()) {
+            }
+            button(">>>Next") {
+                action {
+                    if (chapterNum < store.book.chapters.size)
+                        chapterNum += 1
+                    else
+                        chapterNum = 1
+                }
+            }
+
         }
     }
 }
